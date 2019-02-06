11f5a9ed926858e209aece385f0a35cf9205692a
Janyl Jumadinova <jjumadinova@allegheny.edu>
2019-02-06 12:11:26 -0500

    lab 2 starter files, gradle build fails

---
+++ src/main/java/labtwo/DisplayGraphicalScene.java
@@ -0,0 +1,26 @@
+package labtwo;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.util.Date;
+
+
+public class DisplayGraphicalScene extends Application {
+
+  private static final int SCENE_WIDTH = 600;
+  private static final int SCENE_HEIGHT = 400;
+
+  public void start(Stage primaryStage) {
+
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
